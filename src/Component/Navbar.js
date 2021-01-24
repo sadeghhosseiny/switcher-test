3d164d4b0087ed050a8be0b2a86381e4d1661288
sadegh <thesadegh.h@gmail.com>
2021-01-24 14:58:38 +0330

    still working

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -8,17 +8,42 @@ function Navbar() {
 
         <nav className={`navbar navbar-dark ${styles.navbar}`}>
             <img src={logo} className={`${styles.logo} navbar-brand mr-5`} />
-            <div className="d-flex flex-grow-1">
-                <form className="mx-auto w-75 d-inline-block order-1">
+            <div className={`d-flex flex-grow-1 justify-content-center ${styles.searchForm}`}>
+                <form className="w-75">
                     <div className="input-group">
                         <input type="text" className="form-control py-3" />
-                        <div class="input-group-append">
+                        <div class="input-group-append mr-0">
                             <button className="btn btn-warning" type="submit">
                                 <i className="fas fa-search" />
                             </button>
                         </div>
                     </div>
                 </form>
+            </div>
+            <div className="d-flex mr-5">
+                <div className="col-lg-1 pt-3 mr-5">
+
+                    <span className="mr-5">
+                        <i className="fas fa-flag text-white" />
+                    </span>
+
+                </div >
+                <div className="col-lg-5 mx-auto">
+
+                    <div className="m-auto text-white">
+                        Hello, Sign In
+                </div>
+                    <span className="text-capitalize text-white font-weight-bold">account & list</span>
+
+                </div>
+                <div className="col-lg-4 mx-auto">
+
+                    <div className="text-white">
+                        Returns
+                </div>
+                    <span className="text-white font-weight-bold">& Orders</span>
+                </div>
+
             </div>
             {/* <ul className="navbar-nav">
                 <li className="nav-item mx-3 mt-4 text-light">
