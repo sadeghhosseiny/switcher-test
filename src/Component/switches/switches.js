27b70d12abaa29a1f76587df37b9dfe24faa584a
sadegh hosseini <thesadegh.h@gmail.com>
2021-11-19 15:04:56 +0330

    delete additional codes

--- src/Component/switches/switches.js
+++ src/Component/switches/switches.js
@@ -4,15 +4,14 @@ import ToggleButton from '../toggleButton/toggleButton';
 function Switches() {
 
   const [checkedList, setCheckedList] = useState([]);
-  const [cs, setCs] = useState([]);
 
   const switches = [
     { label: 'GOOD', switch: 'sw1' },
     { label: 'FAST', switch: 'sw2' },
     { label: 'CHEAP', switch: 'sw3' }
   ];
-  const _switches = switches.map(item => (
-    <ToggleButton cs={cs} setCs={setCs} checkedList={checkedList} setCheckedList={setCheckedList} item={item} />
+  const _switches = switches.map((item, i) => (
+    <ToggleButton key={i} checkedList={checkedList} setCheckedList={setCheckedList} item={item} />
   ));
 
   return (
--- src/Component/toggleButton/toggleButton.js
+++ src/Component/toggleButton/toggleButton.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import styles from './toggleButton.module.css';
 
-function ToggleButton({ checkedList, setCheckedList, item, cs, setCs }) {
+function ToggleButton({ checkedList, setCheckedList, item }) {
 
   const handleCheckboxChange = (value, cbName) => {
-    console.log(value, cbName);
-    console.log(checkedList);
     if (!value) {
       setCheckedList((prev) => prev.filter((item) => item !== cbName));
       return;
     }
 
     let tempChecklist = [...checkedList];
-    setCs(cbName);
     tempChecklist.push(cbName);
-    console.log('tmep ', tempChecklist);
     tempChecklist.sort();
     if (tempChecklist.length === 3) {
       if (cbName == 'CHEAP') {
@@ -44,9 +40,8 @@ function ToggleButton({ checkedList, setCheckedList, item, cs, setCs }) {
         checked={checkedList.includes(item.label)}
         onChange={(e) => handleCheckboxChange(e.target.checked, item.label)}
       />
-      {console.log('ergsfs', checkedList.includes('GOOD'))}
-      {console.log('ergsfs', cs)}
-      <label className={`${checkedList.includes(item.label) ? styles[switchClasses[item.label]] : 'custom-control-label'} custom-control-label`} for={item.switch}>
+      <label className={`${checkedList.includes(item.label) ? styles[switchClasses[item.label]] : 'custom-control-label'} custom-control-label`}
+        htmlFor={item.switch}>
         {item.label}
       </label>
     </div>
